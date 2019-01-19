4e1fe86e120468fe3a1d74a118eb2dee31dbfebc
William Gan <wjgan@berkeley.edu>
2019-01-18 23:25:46 -0800

    Internationalization Support (#5)

--- ext/maga.py
+++ ext/maga.py
@@ -4,8 +4,8 @@ async def maga(message):
     if message.author.bot:
         return
 
-    if message.content == 'maga':
-        await message.channel.send('maga')
+    if message.content.lower() == 'maga':
+        await message.channel.send(message.content)
 
 def setup(bot):
     bot.add_listener(maga, 'on_message')

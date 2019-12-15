1c91d3a210dadfddfb9ba15218f114f2dea3f740
Lawrence Liu <lliu301@gmail.com>
2019-12-14 19:59:27 -0600

    add pin

---
+++ ext/pin.py
@@ -0,0 +1,16 @@
+import discord
+from discord.ext import commands
+from http.client import HTTPException
+import typing
+
+pin_emoji = '📌'
+
+async def pin(reaction: discord.Reaction, user: typing.Union[discord.Member, discord.User]) -> None:
+    if str(reaction.emoji) == pin_emoji:
+        try:
+            await reaction.message.pin()
+        except HTTPException as e:
+            await reaction.message.channel.send('Could not pin message, ', e)
+
+def setup(bot) -> None:
+    bot.add_listener(pin, 'on_reaction_add')
--- lendbot.py
+++ lendbot.py
@@ -18,6 +18,7 @@ extensions = [
     'ext.poker',
     'ext.minecraft',
     'ext.ping',
+    'ext.pin',
 ]
 
 class Lendbot(commands.Bot):

9b54cc788fb6d12082f6d41d14032dcede31dad9
Kevin Du <5475211+kevin1055@users.noreply.github.com>
2020-03-07 11:00:35 -0800

    Fix stonks (#30)
    * Fix stonks
    
    * fix

--- util/market.py
+++ util/market.py
@@ -1,25 +1,31 @@
-from datetime import date, datetime
+from datetime import date, datetime, timedelta
+import pytz.reference
 from io import BytesIO
 
-from iexfinance.stocks import Stock, get_historical_data, get_historical_intraday
+import yfinance as yf
+import matplotlib.dates as mdates
 import matplotlib.pyplot as plt
+import matplotlib.ticker as mticker
 import pandas as pd
 
 def get_stock_price(symbol):
-    return Stock(symbol).get_price()
+    info = yf.Ticker(symbol).info
+    return (info['ask'] + info['bid']) / 2
 
 def get_intraday_graph(symbol):
+    now = datetime.now()
     today = date.today()
-    df = get_stock_intraday(symbol, today)
-    df = df[df.average != -1]
-    df.index = pd.to_datetime(df.index)
-    df['average'].plot()
+    df = yf.download(symbol, datetime.now() - timedelta(days=7), interval='5m')
+    last_business_day = df.iloc[[-1]].index.floor(freq='d')[0]
+    df = df.truncate(before=pandas.Timestamp(ts_input=last_business_day))
+    ax = df['Close'].plot()
+    ax.xaxis.set_major_formatter(mdates.DateFormatter("%I:%M %p"))
+    ax.xaxis.set_minor_formatter(mdates.DateFormatter("%I:%M %p"))
+    ax.yaxis.set_major_formatter(mticker.FormatStrFormatter('$%1.2f'))
+
     plt.title(symbol)
     buf = BytesIO()
     plt.savefig(buf, format='png')
     plt.clf()
     buf.seek(0)
     return buf
-
-def get_stock_intraday(symbol, date):
-    return get_historical_intraday(symbol, date, output_format='pandas')

42c8d3f5314600d6df05d1e1351d01845433831b
Kevin Du <5475211+kevin1055@users.noreply.github.com>
2020-03-11 13:42:05 -0700

    Improve karma command

--- ext/karma.py
+++ ext/karma.py
@@ -15,21 +15,29 @@ class Karma(commands.Cog):
     @commands.Cog.listener()
     async def on_message(self, message: discord.Message):
         match = pattern.search(message.content)
-
         if match == None:
             return
 
-        direction = 1 if match.groups()[0] == '++' else -1
+        # Update karma_dict
+        karma_verb = match.groups()[0]
+        karma_change = 1 if karma_verb == '++' else -1
 
-        karma_change = 1
+        recipients = [m for m in message.mentions if m != message.author]
+        for member in recipients:
+            key = str(member.id)
+            self.karma_dict[key] = self.karma_dict.get(key, 0) + karma_change
 
-        for member in message.mentions:
-            if str(member.id) not in self.karma_dict:
-                self.karma_dict[str(member.id)] = 0
-            self.karma_dict[str(member.id)] += karma_change * direction
-
-        if message.mentions != 0:
+        # Write karma_dict to disk if changes
+        if recipients != 0:
             self.write_to_file(self._filename, self.karma_dict)
+
+        # Send out responses
+        response = []
+        sender = message.author.display_name
+        for member in recipients:
+            receiver = member.display_name
+            total = self.karma_dict[str(member.id)]
+            await message.channel.send("{} {}'d {} (now at {})".format(sender, karma_verb, receiver, total))
 
     @commands.command()
     async def karma(self, ctx: discord.ext.commands.Context, member: discord.Member):

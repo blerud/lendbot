be9499d528042e7bb65c0bdcd2e3dd2893756a55
Lawrence Liu <lliu301@gmail.com>
2023-03-17 16:40:49 -0500

    add usernames to history

--- ext/chat.py
+++ ext/chat.py
@@ -21,7 +21,7 @@ def create_listener(bot):
             return
 
         history = await retrieve_history(message.channel)
-        response = retrieve_chat(message.content, history, bot)
+        response = retrieve_chat(message, history, bot)
         await message.channel.send(response)
 
     return chat
@@ -46,21 +46,25 @@ async def retrieve_history(channel):
     return context[reset_index:]
 
 
-def retrieve_chat(msg: str, history: List[discord.Message], bot):
+def retrieve_chat(msg: discord.Message, history: List[discord.Message], bot):
     messages = [{"role": "system", "content": PROMPT}]
     for message in history:
         if message.content is None or len(message.content) == 0:
             continue
-        messages.append(create_context_message(message, bot))
-    messages.append({"role": "user", "content": msg})
+        messages.append(create_openai_message(message, bot))
+    messages.append(create_openai_message(msg, bot))
+    for m in messages:
+        print(m)
     print(messages)
     response = openai.ChatCompletion.create(model=MODEL, messages=messages)
     return response["choices"][0]["message"]["content"]
 
 
-def create_context_message(message: discord.Message, bot):
+def create_openai_message(message: discord.Message, bot):
     role = "assistant" if message.author.id == bot.user.id else "user"
-    return {"role": role, "content": message.content}
+    user = message.author.display_name
+    content = f"{user}: {message.clean_content}"
+    return {"role": role, "content": content}
 
 
 def setup(bot):

074d20ee11d17e033deace749e89ce4841d6b756
Lawrence Liu <lliu301@gmail.com>
2023-03-17 16:20:56 -0500

    add history

--- ext/chat.py
+++ ext/chat.py
@@ -1,3 +1,5 @@
+from typing import List
+
 import discord
 import openai
 
@@ -7,6 +9,8 @@ openai.api_key = openai_key
 
 MODEL = "gpt-3.5-turbo"
 PROMPT = "You are a helpful assistant."
+CONTEXT = 10
+RESET_HISTORY = ".reset"
 
 
 def create_listener(bot):
@@ -16,7 +20,8 @@ def create_listener(bot):
         if not mentioned(bot, message):
             return
 
-        response = retrieve_chat(message.content)
+        history = await retrieve_history(message.channel)
+        response = retrieve_chat(message.content, history, bot)
         await message.channel.send(response)
 
     return chat
@@ -29,11 +34,33 @@ def mentioned(bot, message: discord.Message):
     return False
 
 
-def retrieve_chat(msg: str):
-    response = openai.ChatCompletion.create(
-        model=MODEL, messages=[{"role": "system", "content": PROMPT}, {"role": "user", "content": msg}]
-    )
+async def retrieve_history(channel):
+    context = []
+    async for message in channel.history(limit=CONTEXT):
+        context.insert(0, message)
+    reset_index = 0
+    for i, message in enumerate(context):
+        if RESET_HISTORY in message.content:
+            reset_index = i
+    reset_index += 1
+    return context[reset_index:]
+
+
+def retrieve_chat(msg: str, history: List[discord.Message], bot):
+    messages = [{"role": "system", "content": PROMPT}]
+    for message in history:
+        if message.content is None or len(message.content) == 0:
+            continue
+        messages.append(create_context_message(message, bot))
+    messages.append({"role": "user", "content": msg})
+    print(messages)
+    response = openai.ChatCompletion.create(model=MODEL, messages=messages)
     return response["choices"][0]["message"]["content"]
+
+
+def create_context_message(message: discord.Message, bot):
+    role = "assistant" if message.author.id == bot.user.id else "user"
+    return {"role": role, "content": message.content}
 
 
 def setup(bot):
